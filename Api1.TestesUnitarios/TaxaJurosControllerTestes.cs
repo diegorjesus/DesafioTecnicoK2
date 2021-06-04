f63d1c181fb73109229fd5b5870021bfa0b8ee3a
Diego Jesus <diego.jesus@projetas.com.br>
2021-06-03 23:42:46 -0300

    feat: busca taxa de juros

---
+++ Api1.TestesUnitarios/TaxaJurosControllerTestes.cs
@@ -0,0 +1,26 @@
+﻿using Api1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Api1.TestesUnitarios
+{
+    public class TaxaJurosControllerTestes
+    {
+        private readonly TaxaJurosController _controller;
+
+        public TaxaJurosControllerTestes()
+        {
+            _controller = new TaxaJurosController();
+        }
+        [Fact(DisplayName = "Buscar Taxa de Juros")]
+        public void BuscarTaxaJuros_UmPorcento_DeveRetornarValor()
+        {
+            //Act
+            var resultado = _controller.BuscarTaxaJuros().Result as OkObjectResult;
+
+            //Assert
+            Assert.Equal(0.01, resultado.Value);
+
+        }
+    }
+}
