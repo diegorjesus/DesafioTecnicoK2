f40179af9a0d2222de42635dcb5679a067aab429
Diego Jesus <diego.jesus@projetas.com.br>
2021-06-04 22:45:22 -0300

    test: teste fluxo basico do metodo CalcularJuros

---
+++ Api2.TestesUnitarios/CalculaJurosControllerTestes.cs
@@ -0,0 +1,34 @@
+﻿using Api2.Controllers;
+using Api2.TestesUnitarios.Fixtures;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api2.TestesUnitarios
+{
+    [Collection(nameof(CalculaJurosControllerCollection))]
+    public class CalculaJurosControllerTestes
+    {
+        private readonly CalculaJurosControllerFixture _fixture;
+        private readonly CalculaJurosController _controller;
+
+        public CalculaJurosControllerTestes(CalculaJurosControllerFixture fixture)
+        {
+            _fixture = fixture;
+            _controller = fixture.InstanciarController();
+        }
+
+        [Fact(DisplayName = "Calcula os Juros e retorna ok")]
+        public async Task CalculaJurosAsync_DeveRetornarSucesso()
+        {
+            //Act
+            var resultado = await _controller.CalcularJuros();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(resultado);
+        }
+    }
+}
---
+++ Api2/Controllers/CalculaJurosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Api2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CalculaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> CalcularJuros()
+        {
+            try
+            {
+                return Ok(105.10);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+    }
+}
