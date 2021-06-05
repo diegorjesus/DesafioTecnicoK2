d34a86c9cb2a9c15b371ae2b58568ac7c4d27f3a
Diego Jesus <diego.jesus@projetas.com.br>
2021-06-04 21:10:01 -0300

    feat: retorna a url do git onde o fonte se encontra

---
+++ Api2/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Api2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+        private readonly string _gitUrl;
+
+        public ShowMeTheCodeController(IConfiguration config)
+        {
+            _gitUrl = config.GetSection("Urls").GetValue<string>("GitUrl");
+        }
+
+        [HttpGet]
+        [SwaggerResponse(200, "Retorna Url do gitHub onde o fonte se encontra")]
+        public ActionResult<string> GetGitHubUrl()
+        {
+            return Ok(_gitUrl);
+        }
+    }
+}
