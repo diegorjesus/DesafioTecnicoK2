ec252dfcde08782513e08c847819fab6b9276216
Diego Jesus <diego.jesus@projetas.com.br>
2021-06-04 21:10:42 -0300

    refactor: elimina comentários desnecessários do código

--- Api1/Controllers/TaxaJurosController.cs
+++ Api1/Controllers/TaxaJurosController.cs
@@ -8,9 +8,6 @@ namespace Api1.Controllers
     [ApiController]
     public class TaxaJurosController : ControllerBase
     {
-        /// <summary>
-        /// Buscar Taxa de Juros
-        /// </summary>
         [HttpGet]
         [SwaggerResponse(200, "Retorna taxa de juros de 0.01")]
         public ActionResult<double> BuscarTaxaJuros()
