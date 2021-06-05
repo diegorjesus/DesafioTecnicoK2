985601bb39fa8eb24517fbbacf6451ffdfe5c653
Diego Jesus <diego.jesus@projetas.com.br>
2021-06-05 00:06:06 -0300

    test: valida retorno 500 quando serviço retorna excessão

--- Api2.TestesUnitarios/CalculaJurosControllerTestes.cs
+++ Api2.TestesUnitarios/CalculaJurosControllerTestes.cs
@@ -1,4 +1,5 @@
-﻿using Api2.Controllers;
+﻿using Api2.Application.Interfaces;
+using Api2.Controllers;
 using Api2.TestesUnitarios.Fixtures;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -21,14 +22,47 @@ namespace Api2.TestesUnitarios
             _controller = fixture.InstanciarController();
         }
 
-        [Fact(DisplayName = "Calcula os Juros e retorna ok")]
-        public async Task CalculaJurosAsync_DeveRetornarSucesso()
+        [Fact(DisplayName = "Calcula os Juros e retorna sucesso")]
+        public async Task CalculaJurosAsync_DeveRetornarValor()
         {
+            //Arrange
+            var parametros = new ParametrosConsulta
+            {
+                ValorInicial = 100,
+                Tempo = 5
+            };
+
+            _fixture.Mocker.GetMock<ICalcularJurosAsyncAppService>()
+                .Setup(x => x.CalcularJurosAsync(parametros))
+                .Returns(Task.FromResult(105.10));
+
             //Act
-            var resultado = await _controller.CalcularJuros();
+            var resultado = await _controller.CalcularJuros(parametros);
 
             //Assert
             Assert.IsType<OkObjectResult>(resultado);
+            Assert.Equal(105.1, (resultado as OkObjectResult).Value);
+        }
+
+        [Fact(DisplayName = "Quando servico retornar uma excessão")]
+        public async Task CalculaJurosAsync_ServicoRetornaExcessao_DeveRetornarInternalServerError()
+        {
+            //Arrange
+            var parametros = new ParametrosConsulta
+            {
+                ValorInicial = 100,
+                Tempo = 5
+            };
+
+            _fixture.Mocker.GetMock<ICalcularJurosAsyncAppService>()
+                .Setup(x => x.CalcularJurosAsync(parametros))
+                .Returns(Task.FromException<double>(new Exception()));
+
+            //Act
+            var resultado = await _controller.CalcularJuros(parametros);
+
+            //Assert
+            Assert.Equal(500, (resultado as ObjectResult).StatusCode);
         }
     }
 }
