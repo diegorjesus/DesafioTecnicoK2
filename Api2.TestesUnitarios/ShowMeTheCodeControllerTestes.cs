e815d4b0b670bb6fc3131960458e0afbc912bdfa
Diego Jesus <diego.jesus@projetas.com.br>
2021-06-04 22:11:07 -0300

    test: testa método responsável por exibir url do git onde o fonte se encontra

---
+++ Api2.TestesUnitarios/ShowMeTheCodeControllerTestes.cs
@@ -0,0 +1,29 @@
+﻿using Api2.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Api2.TestesUnitarios
+{
+    public class ShowMeTheCodeControllerTestes
+    {
+        private readonly ShowMeTheCodeController _controller;
+
+        public ShowMeTheCodeControllerTestes()
+        {
+            _controller = new ShowMeTheCodeController();
+        }
+
+        [Fact(DisplayName = "Buscar Url do git onde o fonte se encontra")]
+        public void GetGitHubUrl_DeveRetornarValor()
+        {
+            //Arrange
+            var gitUrlExpected = "https://github.com/diegorjesus/DesafioTecnicoK2.git";
+
+            //Act
+            var resultado = _controller.GetGitHubUrl().Result as OkObjectResult;
+
+            //Assert
+            Assert.Equal(gitUrlExpected, resultado.Value);
+        }
+    }
+}
