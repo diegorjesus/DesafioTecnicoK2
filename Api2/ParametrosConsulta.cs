2ab44a910de72ad801b790391c500b2c01e1f0d5
Diego Jesus <diego.jesus@projetas.com.br>
2021-06-05 00:07:22 -0300

    refactor: alterando validação para permitir ValorInicial negativo

--- Api2/ParametrosConsulta.cs
+++ Api2/ParametrosConsulta.cs
@@ -4,7 +4,6 @@ namespace Api2
 {
     public class ParametrosConsulta
     {
-        [Range(0, double.MaxValue, ErrorMessage = "Valor deve ser maior ou igual a 0")]
         public double ValorInicial { get; set; }
 
         [Range(0, int.MaxValue, ErrorMessage = "Valor deve ser maior ou igual a 0")]
